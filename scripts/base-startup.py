03d10e083f40614855e661c6d361bb741a2390a0
colinthesealion <walaruigio@athenahealth.com>
2018-07-02 20:31:34 -0700

    Fixed syntax error

--- scripts/base-startup.py
+++ scripts/base-startup.py
@@ -8,7 +8,7 @@ LED_PIN = 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
 
-for x in range(5)
+for x in range(5):
   GPIO.output(LED_PIN, GPIO.HIGH)
   sleep(0.5)
   GPIO.output(LED_PIN, GPIO.LOW)
